2495385fffc5bbd2f774c3ac5a1354578610b041
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-06-01 15:10:23 +0530

    chore : test

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -7,14 +7,14 @@ const Contact = () => {
       {/* <div className=" flex justify-center items-center text-white text-3xl font-bold w-[900px] p-4">
         WANT TO REACH OUT TO US?
       </div> */}
-      <div className="flex md:flex-row flex-col md:w-[1100px]  w-full justify-between items-center p-16">
+      <div className="flex md:flex-row flex-col md:w-[1100px]  w-full justify-between items-center xs:p-16 p-2">
     <div className="md:w-[600px] flex flex-col justify-center items-center w-full p-4 md:text-6xl text-4xl font-bold text-white" >
       <p className="w-full">CONTACT US FROM </p><p className="flex w-full text-yellow-400">ANY PART OF THE WORLD</p>
     </div>
       <form action="" className="h-96 flex flex-col justify-between px-4 py-6 md:w-96 w-full">
-        <input className="w-80 p-2 rounded-xl" type="text" name="name" placeholder="Your good name" />
-        <input className="w-80 p-2 rounded-xl" type="email" name="email" placeholder="Your E-mail Address" />
-        <textarea name="message" className="h-32 w-96 p-2 rounded-xl" placeholder="Your Message" />
+        <input className="md:w-80 w-full p-2 rounded-xl" type="text" name="name" placeholder="Your good name" />
+        <input className="md:w-80 w-full p-2 rounded-xl" type="email" name="email" placeholder="Your E-mail Address" />
+        <textarea name="message" className="h-32 md:w-96 w-full p-2 rounded-xl" placeholder="Your Message" />
         <button className="w-32 h-10 p-2 bg-white rounded-xl  text-black">
               Submit
         </button>
