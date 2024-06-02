59bfcbe6ed9bee77e99853d2a246a358d6a92348
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-06-02 18:28:21 +0530

    push

--- src/Pages/Index.tsx
+++ src/Pages/Index.tsx
@@ -1,8 +1,14 @@
 
 import Header from '../Components/Header'
 import Welcome from '../Components/Welcome'
-
+import { useEffect } from 'react';
 const Index = () => {
+  useEffect(() => {
+    fetch('/.netlify/functions/hello')
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
   return (
     <>
         <Header width='full' active="Home" isLoggedIn={false} />
