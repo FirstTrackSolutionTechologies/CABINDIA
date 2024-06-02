0a58e237c5e0ac1a276689984719eb1199420b38
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-06-02 17:36:15 +0530

    push

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -32,10 +32,10 @@ const Contact = () => {
         WANT TO REACH OUT TO US?
       </div> */}
       <div className="flex lg:flex-row flex-col  w-full justify-center items-center xs:p-16 p-2">
-    <div className=" flex flex-1 flex-col justify-center md:w-1/2 w-full p-4 text-4xl lg:text-6xl  font-bold text-white" >
+    <div className=" flex flex-col justify-center md:w-1/2 w-full p-4 text-4xl lg:text-6xl  font-bold text-white" >
       <p className=" text-center lg:w-auto lg:block flex justify-center w-full ">CONTACT US FROM </p><p className=" text-center lg:w-auto lg:block flex justify-center w-full text-yellow-400">ANY PART OF THE WORLD</p>
     </div>
-      <form onSubmit={handleSubmit} action="" className="h-96 flex flex-col sm:w-1/2 px-6 justify-between py-6 md:w-96 w-full text-yellow-400">
+      <form onSubmit={handleSubmit} action="" className="h-96 flex flex-col sm:w-3/4 px-6 justify-between py-6 md:w-96 w-full text-yellow-400">
         <input value={formData.name} onChange={handleChange} required className="md:w-80 w-full p-2 rounded-xl bg-gray-800" type="text" name="name" placeholder="Your good name" />
         <input value={formData.email} onChange={handleChange} required className="md:w-80 w-full p-2 rounded-xl bg-gray-800" type="email" name="email" placeholder="Your E-mail Address" />
         <textarea value={formData.message} onChange={handleChange} required name="message" className="h-32 md:w-96 w-full p-2 rounded-xl bg-gray-800" placeholder="Your Message" />
