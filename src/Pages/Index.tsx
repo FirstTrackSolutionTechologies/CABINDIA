fef620ff433c4b2d81afc5317544a4c0cdad0dd5
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-06-02 18:38:27 +0530

    push

--- src/Pages/Index.tsx
+++ src/Pages/Index.tsx
@@ -2,7 +2,7 @@
 import Header from '../Components/Header'
 import Welcome from '../Components/Welcome'
 const Index = () => {
-    fetch('/.netlify/functions/hello')
+    fetch('/api/hello')
       .then(response => response.json())
       .then(data => alert(data.message))
       .catch(error => alert('Error fetching data:'+ error));
