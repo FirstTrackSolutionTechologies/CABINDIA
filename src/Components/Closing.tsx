f9b9815c0f8bc0851c58f3f22c0fdbbdb60ac675
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-06-04 16:13:33 +0530

    hello

--- src/Components/Closing.tsx
+++ src/Components/Closing.tsx
@@ -3,7 +3,7 @@
 const Closing = () => {
   return (
     <div className='bg-black text-white w-full flex flex-col sm:flex-row justify-center items-center text-xs sm:text-md md:text-lg text-center py-3'>
-      Copyright 2024, developed by &nbsp; <p><a href="mailto:adityakumar5155@gmail.com">AdityaKumar5155</a></p>  &nbsp; from  &nbsp; <p><a href="https://firsttracksolution.tech">First Track Solutions Pvt. Ltd</a></p>
+      Copyright 2024, developed by &nbsp; <p><a href="https://firsttracksolution.tech">First Track Solutions Pvt. Ltd</a></p>
     </div>
   )
 }
